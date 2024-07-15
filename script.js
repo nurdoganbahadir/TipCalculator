e4d5a647ce49442d2bbee537f4408c600d7bfe69
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-15 15:31:43 +0300

    html update and script created

---
+++ script.js
@@ -0,0 +1,12 @@
+const billAmount = document.querySelector("#billAmount");
+const tipPer = document.querySelector("#tipPer");
+const result = document.querySelector("#result");
+const calculate = document.querySelector("#calculate");
+
+let sonuc = Number(billAmount)+(Number(billAmount)*Number(tipPer)/100)
+
+
+
+
+
+sonuc.textContent("#result")
